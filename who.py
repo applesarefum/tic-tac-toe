7bf22bfbacc9a32daafaaadb3bf01c6b50b88017
William Muhlbach <applesarefum@gmail.com>
2017-03-31 12:55:36 -0400

    I did a thing for who, but it's weird

--- who.py
+++ who.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
+from getch import getch
+print("Will you play as X or O?\nPress A for X\nPress D for O")
+if getch()=="a":
+    print("You chose X!")
+if getch()=="d":
+    print("You chose O!")
 
