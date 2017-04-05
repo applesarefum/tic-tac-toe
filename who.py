a654991f4f1f34a224a8cbd4626af256d2c8cde4
William Muhlbach <applesarefum@gmail.com>
2017-04-05 17:38:26 -0400

    save

--- who.py
+++ who.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 from getch import getch
 print("Will you play as X or O?\nPress A for X\nPress D for O")
-if getch()=="a":
+meme=getch()
+if meme=="a":
     print("You chose X!")
-if getch()=="d":
+if meme=="d":
     print("You chose O!")
 
